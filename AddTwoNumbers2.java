2d4a672989b87be1c70faa0723191b359bb8188e
graphicsdesignpoint <77166165+graphicsdesignpoint@users.noreply.github.com>
2022-06-19 23:44:13 +0600

    Add files via upload

---
+++ AddTwoNumbers2.java
@@ -0,0 +1,230 @@
+import java.util.Scanner;
+public class AddTwoNumbers2 {
+
+    public static void main(String[] args) {
+		
+		
+		
+		/*	
+			
+		// series:- 1 + 2 + 3 + 4 + ... + N
+
+		Scanner scan = new Scanner(System.in);
+
+		// declare variables
+		int n;
+		int sum = 0;
+
+		// read N value
+		System.out.print("Enter N value: ");
+		n = scan.nextInt();
+
+		// calculate the sum of series
+		for(int i=1; i<=n; i++) {
+		  sum = sum + i;
+		}
+
+		// display result
+		System.out.println("Sum = " + sum);
+
+		
+
+		
+		
+		
+		
+		
+	// series:- 1*1 + 2*2 + 3*3 + 4*4 + 5*5 +….+ n*n
+
+    // create Scanner class object
+    // to read input values
+    Scanner scan = new Scanner(System.in);
+
+    // declare variables
+    int n;
+    int sum = 0;
+
+    // read N value
+    System.out.print("Enter N value: ");
+    n = scan.nextInt();
+
+    // calculate the sum of series
+    for(int i=1; i<=n; i++) {
+      sum = sum + i*i;
+    }
+
+    // display result
+    System.out.println("Sum = " + sum);
+
+		
+		
+		
+		
+		
+		
+		// array and print index number: 
+		int arr[][]={{1,2,3},{2,4,5},{4,4,5}};  
+		//printing 2D array  
+		for(int i=0;i<3;i++){  
+		for(int j=0;j<3;j++){  
+		System.out.print(arr[i][j]+" ");  
+		}  
+		System.out.println();  
+		}
+
+		
+		
+		
+		
+		//Odd or Even Checking
+		
+	    int num = 10;
+        if (num % 2 == 0) {
+            System.out.println("Entered Number is Even");
+        }
+ 
+        else {
+            System.out.println("Entered Number is Odd");
+        }	
+		
+		
+		
+		//Odd or Even Chaking 2nd way
+		
+		int n = 100;
+ 
+        // Condition check
+        // if n|1 if greater than n then this number is even
+        if ((n | 1) > n) {
+ 
+            // Print statement
+            System.out.println("Number is Even");
+        }
+        else {
+ 
+            // Print statement
+            System.out.println("Number is Odd");
+        }
+		
+		
+		
+		
+	 //Reverse a number using a for loop
+	 
+	 int num = 1234567, reversed = 0;
+
+    for(;num != 0; num /= 10) {
+      int digit = num % 10;
+      reversed = reversed * 10 + digit;
+    }
+
+    System.out.println("Reversed Number: " + reversed);
+	 
+	 
+	 
+	 
+		
+		
+	 //Reverse a Number using a while loop
+	 
+	  int num = 1234, reversed = 0;
+    
+    System.out.println("Original Number: " + num);
+
+    // run loop until num becomes 0
+    while(num != 0) {
+    
+      // get last digit from num
+      int digit = num % 10;
+      reversed = reversed * 10 + digit;
+
+      // remove the last digit from num
+      num /= 10;
+    }
+
+    System.out.println("Reversed Number: " + reversed);
+		
+		
+        
+		
+		
+		//Print the sum, multiply, subtract, divide and remainder of two numbers
+				
+		Scanner in = new Scanner(System.in);
+
+		System.out.print("Input first number: ");
+		int num1 = in.nextInt();
+
+		System.out.print("Input second number: ");
+		int num2 = in.nextInt();
+
+
+		System.out.println(num1 + " + " + num2 + " = " + 
+		(num1 + num2));
+
+		System.out.println(num1 + " - " + num2 + " = " + 
+		(num1 - num2));
+
+		System.out.println(num1 + " x " + num2 + " = " + 
+		(num1 * num2));
+
+		System.out.println(num1 + " / " + num2 + " = " + 
+		(num1 / num2));
+
+		System.out.println(num1 + " mod " + num2 + " = " + 
+		(num1 % num2));
+		
+		
+		
+		
+		
+		//Another Way
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Input the first number: ");
+		int n1 = scanner.nextInt();
+		System.out.println("Input the second number: ");
+		int n2 = scanner.nextInt();
+		int sum = n1 + n2;
+		int minus = n1 - n2;
+		int multiply = n1 * n2;
+		int subtract = n1 + n2;
+		int divide = n1 / n2;
+		int rnums = n1 % n2;
+		System.out.printf("Sum = %d\nMinus = %d\nMultiply = %d\nSubtract = %d\nDivide = %d\nRemainderOf2Numbers = %d\n ", sum, minus, multiply, subtract, divide, rnums);
+		
+		
+		
+		
+
+	
+	
+		//Sum of two numbers
+		
+	  int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+		
+		
+		
+		// sum of two number using scanner
+		
+        int num1, num2, sum;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number: ");
+        num1 = sc.nextInt();
+        
+        System.out.println("Enter Second Number: ");
+        num2 = sc.nextInt();
+        
+        sc.close();
+        sum = num1 + num2;
+        System.out.println("Sum of these numbers: "+sum);
+		
+		*/
+		
+		
+	
+		
+    }
+}
